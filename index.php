0a8bed034546bac04d5454fe986ee9ce833983dc
shravan97 <shravanmurali@gmail.com>
2016-04-06 14:31:26 +0530

    added id to dept list

--- index.php
+++ index.php
@@ -168,7 +168,7 @@ if(!defined('__PRAGYAN_CMS'))
                         <li>
                             <a id="d" href="#" title="" class="department"><i class="fa fa-toggle-off"></i> Department</a>
                             <script>$('#d').click(function(){$(this).find('i').toggleClass('fa-toggle-off fa-toggle-on');});</script>
-                            <ul class="sub-menu visible-xs">
+                            <ul id='department-list' class="sub-menu visible-xs">
                                 <li><a href="#">Computer Science & Engineering</a></li>
                                 <li><a href="#">Electrical & Electronics Engineering</a></li>
                                 <li><a href="#">Electronics & Communication Engineering</a></li>
