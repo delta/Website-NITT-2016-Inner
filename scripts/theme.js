8df22b59e95dd79d6e6f2fdd0f009147322c0d1c
shravan97 <shravanmurali@gmail.com>
2016-04-13 02:06:54 +0530

    changed class reference to id

--- scripts/theme.js
+++ scripts/theme.js
@@ -38,7 +38,7 @@
 		menucloner();
 		if( CHEF.mobileMenuClone!='' ) {
 			CHEF.mobileMenuClone.insertAfter( '[id="k-menu affix"]' );
-			$('.k-main-navig').hide();
+			$('[id="k-menu affix"]').hide();
 
 			$( 'nav#navigation-mobile' ).removeClass( 'k-main-navig' );
 			$( 'nav#navigation-mobile > ul' ).removeAttr( 'id' ).removeClass( 'k-dropdown-menu' ).addClass( 'list-unstyled' );
