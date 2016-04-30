d12db17ec57e00fa4e609091be103e64d55777d3
Sachin <ediblepeople@gmail.com>
2016-04-30 17:02:40 +0530

    Sliding menu's height is fixed to window height 	Scrolling on lower divs cancelled on sliding menu:active 	Changed background image(scales according to width) and color 	Sidebar scroll happens only for the sideMenu unordered list

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -6,12 +6,16 @@
 		var $page = $(".whitespace");
 		var $footer = $(".footer");
 		var $header = $("header");
+		var $body = $("body");
 		//var mh=$page.height()+$footer.height()+$header.height();
 		var mh=$("body").outerHeight();
 		console.log(mh);
+		
+		$('.sidebarslider').children().children().children().children().eq(0).css('height',(3/10)*screen.width);
+		
 		var sidemenu=$('.sidebarslider').children().children().children().children().eq(1);
-		$('.sidebarslider').children().children().children().css('max-height',mh);
-		$('.sidebarslider').children().children().children().css('min-height',mh);
+		$('.sidebarslider').children().children().children().css('max-height',screen.height);
+		$('.sidebarslider').children().children().children().css('min-height',screen.height	);
 		$('.sidebarslider').children().children().children().css('overflow-y','scroll');
 		sidemenu.css('max-width',(3/4)*screen.width);
 		sidemenu.css('min-width',(3/4)*screen.width);
@@ -20,6 +24,10 @@
 		{
 				var $sidebar = $(".MenuSlider");
 				$sidebar.css('display','block');
+				if(parseInt($sidebar.css('left'),10)==0)
+					$body.css('overflow','auto');
+				else
+					$body.css('overflow','hidden');
 				$sidebar.animate({ left: parseInt($sidebar.css('left'),10) == 0 ? -$sidebar.outerWidth(): 0});
 		});
 	});
