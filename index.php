ed79cbffd1107336be045da8a5e7e81d190f8d49
shravan97 <shravanmurali@gmail.com>
2016-04-30 15:18:39 +0530

    styled the content header

--- index.php
+++ index.php
@@ -283,7 +283,7 @@ if(!defined('__PRAGYAN_CMS'))
 			<div id="cms-content">
             <!-- <span id="currpage" style="font-weight:50; -->
             <!-- font-size:small;"></span> -->
-            <h2 id="currpage">
+            <h2 id="currpage" style="margin-top: 0;font-weight: bold;">
                 
             </h2>
             <hr style="background-color: #BDBBBB;height: 1px;margin-bottom:20px;">
